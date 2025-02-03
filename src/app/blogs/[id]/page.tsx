e5206c3ffaa7cad62298ca31416409157e832853
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2025-02-03 04:16:27 -0600

    Fetch dynamic data using generateStaticParams() for SSG;

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,17 +1,30 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "../../../../Type";
 
 interface Params {
   params: {
     id: string;
   };
 }
+
+// Fetch dynamic data using generateStaticParams() for SSG;
+export const generateStaticParams = async () => {
+  const response = await fetch("http://localhost:5000/blogs", {
+    cache: "no-store",
+  });
+  const blogs = await response.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    id: blog.id,
+  }));
+};
+
 const BlogsDetailsPage = async ({ params }: Params) => {
   const response = await fetch(`http://localhost:5000/blogs/${params.id}`, {
     cache: "no-store",
   });
   const blog = await response.json();
   return (
-    <div>
+    <div className="my-3">
       <BlogDetails blog={blog} />
     </div>
   );
