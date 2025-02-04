34695a8099c9f96fb75f19f275070645b9a5411e
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2025-02-04 04:14:12 -0600

    BlogForm get a data and added a id or likes

---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,11 @@
+import CreateBlogForm from "@/components/ui/BlogForm";
+
+const CreateBlogPost = () => {
+  return (
+    <div>
+      <CreateBlogForm />
+    </div>
+  );
+};
+
+export default CreateBlogPost;
